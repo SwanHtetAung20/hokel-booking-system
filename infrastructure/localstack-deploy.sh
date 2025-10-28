4035b83476e98771f0491087b008719db2088d21
Swan Htet Aung <swanhtetaung308@gmail.com>
2025-10-29 00:56:15 +0700

    add script

--- infrastructure/localstack-deploy.sh
+++ infrastructure/localstack-deploy.sh
@@ -2,6 +2,9 @@
 
 set -e # stops the script if a command fails
 
+aws --endpoint-url=http://localhost:4566 cloudformation delete-stack \
+    --stack-name hotel-booking-system
+
 aws --endpoint-url=http://localhost:4566 cloudformation deploy \
     --stack-name hotel-booking-system \
     --template-file "infrastructure/cdk.out/localStack.template.json"
